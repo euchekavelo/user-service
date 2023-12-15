14e2f004a6f562a2c780bbc54e3e30617e837894
Суслов Кирилл Васильевич <Kirill.Suslov@norbit.ru>
2023-12-15 04:06:12 +0300

    The initial user microservice configuration has been created.

---
+++ src/main/java/ru/skillbox/userservice/controller/HelloController.java
@@ -0,0 +1,13 @@
+package ru.skillbox.userservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping
+    public String index() {
+        return "Hello world from users service!";
+    }
+}
