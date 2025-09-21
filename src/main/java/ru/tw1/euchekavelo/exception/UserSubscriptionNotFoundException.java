a70a6e644869843c17c75d7011bbd284e936a659
Kirill Suslov <euchekavelo@gmail.com>
2025-09-22 00:25:19 +0300

    The code base has been refactored.

---
+++ src/main/java/ru/tw1/euchekavelo/exception/UserSubscriptionNotFoundException.java
@@ -0,0 +1,8 @@
+package ru.tw1.euchekavelo.exception;
+
+public class UserSubscriptionNotFoundException extends RuntimeException {
+
+    public UserSubscriptionNotFoundException(String message) {
+        super(message);
+    }
+}
